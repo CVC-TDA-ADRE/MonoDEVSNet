078da4ef742a6e055ccbf7f9fbfe374b1ebe7e90
gvillalonga89 <gvillalonga@cvc.uab.es>
2021-08-31 17:48:06 +0200

    Removed prepare scripts from entrypoint

--- entrypoint_dev.sh
+++ entrypoint_dev.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# compile monodepth2 framework
-cd ${MONODEVSNET_ROOT}/utils
-chmod +x prepare_monodepth2_framework.sh
-./prepare_monodepth2_framework.sh
-cd ..
-
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/install/setup.bash"
 source "/ROS_LIBS/vision_opencv/install/setup.bash"
